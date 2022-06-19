8c7a2e4b53b27725dfa4dd7cfbaf396dd58dc33e
kimine <sanagi1987@163.com>
2022-06-19 17:19:38 +0800

    init

---
+++ .config/fish/conf.d/env.fish
@@ -0,0 +1,11 @@
+go env -w GOPROXY="https://goproxy.cn,direct"
+
+set -g theme_color_scheme gruvbox
+set -g theme_date_format "+%Y-%m-%d %a %H:%M:%S"
+# set -g theme_powerline_fonts no
+
+set -gx EDITOR emacs
+
+set PATH $HOME/bin $HOME/.cargo/bin $PATH $HOME/.emacs.d/bin
+
+alias ip="ip -c"
