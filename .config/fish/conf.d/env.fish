bf09379183b214cd14a67a8145bd965bd3dc4c83
zhengqf <zhengqf@taojinhudong.com>
2022-10-31 12:54:08 +0800

    enable win select

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,7 +2,7 @@ set -g theme_color_scheme gruvbox
 set -g theme_date_format "+%Y-%m-%d %a %H:%M:%S"
 # set -g theme_powerline_fonts no
 
-set -gx EDITOR emacs
+set -gx EDITOR vim 
 
 set PATH $HOME/bin $HOME/.cargo/bin $PATH $HOME/.emacs.d/bin
 
--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,7 +22,9 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "xos4 Terminus" :size 14)
-        doom-variable-pitch-font (font-spec :family "xos4 Terminus" :size 14))
+        doom-variable-pitch-font (font-spec :family "xos4 Terminus" :size 14)
+        doom-big-font (font-spec :family "xos4 Terminus")
+        )
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
