6da5fe70059386d91a2ec8fbcb6515e89a2100f8
zhengqf <zhengqf@taojinhudong.com>
2022-11-25 17:46:57 +0800

    alacritty setting

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -4,7 +4,7 @@ set -g theme_date_format "+%Y-%m-%d %a %H:%M:%S"
 
 set -gx EDITOR vim 
 
-fish_add_path $HOME/bin $HOME/.cargo/bin $HOME/.emacs.d/bin
+fish_add_path $HOME/bin $HOME/.cargo/bin $HOME/.emacs.d/bin $HOME/.local/bin
 
 set -g theme_vcs_ignore_paths /c /d
 
@@ -13,9 +13,8 @@ if type -q rustc
     set -gx RUST_SRC_PATH "$rustroot"/lib/rustlib/src/rust/src/
 end
 
-set -gx TERM alacritty
-set -gx QT_IM_MODULE fcitx
-set -gx GTX_IM_MODULE fcitx
+set -gx TERM xterm-256color
+set -gx TERMINAL alacritty
 set -gx XMODIFIERS @im=fcitx
 
 alias ip="ip -c"
