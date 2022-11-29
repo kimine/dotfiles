75a78e0cfbe01b22586ab578a02fb43a72c2367d
zhengqf <zhengqf@taojinhudong.com>
2022-11-29 13:42:35 +0800

    polybar time

--- .config/fish/functions/zapdf.fish
+++ .config/fish/functions/zapdf.fish
@@ -1,6 +1,6 @@
 function zapdf 
     set -l pdfpath (fd -a -e "pdf" -e "epub" . "$HOME/OneDrive/ebook"| fzf)
     if test -n "$pdfpath"
-        nohup zathura $pdfpath > /dev/null 2>&1 &
+        zathura $pdfpath > /dev/null 2>&1 & disown
     end
 end
