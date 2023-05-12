753d589f2cfa4820be00f252e88242c12f87cfe5
zhengqf <zhengqf@taojinhudong.com>
2023-05-13 05:29:28 +0800

    fzf complete

--- .config/fish/conf.d/fzf_env.fish
+++ .config/fish/conf.d/fzf_env.fish
@@ -10,3 +10,11 @@ set -gx FZF_CTRL_R_OPTS "\
   --bind 'ctrl-y:execute-silent(echo -n {..} | xsel -i -p && xsel -o -p | xsel -i -b)+abort' \
   --color header:bold \
   --header 'Press CTRL-Y to copy command into clipboard'"
+if not bind --user \t >/dev/null 2>/dev/null
+    if set -q FZF_COMPLETE
+        bind \t fzf_complete
+        if ! test "$fish_key_bindings" = fish_default_key_bindings
+            bind -M insert \t fzf_complete
+        end
+    end
+end
